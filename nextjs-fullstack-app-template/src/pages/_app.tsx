8251c156d7bb9bd84158437a339cb69ac686c8b5
hounaikuo <hounaikuo@canaan-creative.com>
2023-03-25 18:33:51 +0800

    layout

--- nextjs-fullstack-app-template/src/pages/_app.tsx
+++ nextjs-fullstack-app-template/src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import { NextPageWithLayout } from './page';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
 }
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+	// 如果这个 layout 是可用的，则在页面中使用 
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
+}
+
+export default MyApp;
